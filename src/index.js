ea3a75511937b9cc368db75b234f066b9e4441e7
Gyulavári Peti <gypeti23@gmail.com>
2019-10-09 13:57:50 +0200

    export new classes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const _modules = [ 'Buyer', 'Client', 'Invoice', 'Item', 'Seller' ]
+const _modules = [ 'Buyer', 'Client', 'Invoice', 'Item', 'Seller', 'CreditEntry', 'CreditItem' ]
 
 _modules.forEach(n => {
   exports[ n ] = require('./lib/' + n)
